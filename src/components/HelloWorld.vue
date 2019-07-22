a9d7392c88035554c6f4f962cf59b1aa25985c79
yukidaruma <yukidaruma@users.noreply.github.com>
2019-07-22 10:37:21 +0900

    Add gif encoding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="hello">
     <div id="animation-container"></div>
-    <video v-if="videoDataUrl" :src="videoDataUrl" autoplay controls loop />
+    <div v-if="recorderType === 'webm'">
+      <video v-if="videoDataUrl" :src="videoDataUrl" autoplay controls loop />
+    </div>
+    <div v-else-if="recorderType === 'gif'">
+      <img v-if="videoDataUrl" :src="videoDataUrl">
+    </div>
+    <div>
+      Video file format:
+      <label><input v-model="recorderType" value="gif" type="radio" :disabled="isRecording" @change="destroyAnimation">Gif</label>
+      <label><input v-model="recorderType" value="webm" type="radio" :disabled="isRecording" @change="destroyAnimation">Webm</label>
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,8 @@ import dragDrop from 'drag-drop';
 import { ungzip } from 'pako';
 import lottie, { AnimationItem } from 'lottie-web';
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import Gif from 'gif.js';
+import workerBlob from '../gif-worker-blob.js';
 
 const loadTgs = (filereader): string | null => {
   const buffer = filereader.result as ArrayBuffer;
@@ -34,10 +46,26 @@ const xmlSerializer = new XMLSerializer();
 @Component
 export default class HelloWorld extends Vue {
   animation: AnimationItem | null = null;
-  removeListner: Function | null = null;
+  gif: Gif | null = null;
   isAnimating: boolean = false;
   videoDataUrl: string = '';
+  removeListner: Function | null = null;
+  recorderType: 'gif' | 'webm' = 'gif';
+  objectUrls: string[] = [];
+  isRecording: boolean = false;
 
+  destroyAnimation() {
+    if (this.animation) {
+      this.animation.destroy();
+    }
+    this.videoDataUrl = '';
+    this.objectUrls.forEach(objectUrl => URL.revokeObjectURL(objectUrl));
+  }
+  createObjectUrl(blob: Blob): string {
+    const objectUrl = URL.createObjectURL(blob);
+    this.objectUrls.push(objectUrl);
+    return objectUrl;
+  }
   loadAnimation(lottieJson: string) {
     const canvas = document.createElement('canvas');
     canvas.height = 320;
@@ -45,15 +73,23 @@ export default class HelloWorld extends Vue {
     const context = canvas.getContext('2d');
     context.fillStyle = '#ffffff';
     const stream = canvas.captureStream();
-    const recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+    const recorder = this.recorderType === 'webm' ? new MediaRecorder(stream, { mimeType: "video/webm" }) : {
+      stop: () => {},
+    };
+    if (this.recorderType === 'gif') {
+      this.gif = this.createGifEncoder();
+    }
     const data = [];
     recorder.ondataavailable = function(event) {
       if (event.data && event.data.size) {
         data.push(event.data);
       }
     };
-    recorder.onstop = () => {
-      this.videoDataUrl = URL.createObjectURL(new Blob(data, { type: "video/webm" }));
+    const $vm = this;
+    recorder.onstop = function() {
+      const blob = new Blob(data, { type: "video/webm" });
+      $vm.isRecording = false;
+      $vm.videoDataUrl = $vm.createObjectUrl(blob);
     };
 
     let animationData;
@@ -64,26 +100,23 @@ export default class HelloWorld extends Vue {
       return;
     }
 
-    if (this.animation) {
-      this.animation.destroy();
-      this.videoDataUrl = '';
-    }
-
+    this.destroyAnimation();
     this.animation = lottie.loadAnimation({
       animationData,
       container: document.getElementById('animation-container'),
       // loop: true,
     });
     this.isAnimating = true;
+    this.isRecording = true;
 
     this.animation.addEventListener('enterFrame', () => {
-      if (recorder.state === 'inactive') {
+      if (this.recorderType === 'webm' && recorder.state === 'inactive') {
         recorder.start();
       }
 
       const svg = document.querySelector('#animation-container svg');
       const xml = xmlSerializer.serializeToString(svg);
-      const dataUrl = URL.createObjectURL(new Blob([xml], {type: "image/svg+xml"}));
+      const dataUrl = this.createObjectUrl(new Blob([xml], {type: "image/svg+xml"}));
       const img = new Image(320, 320);
       img.height = 320;
       img.width = 320;
@@ -91,13 +124,43 @@ export default class HelloWorld extends Vue {
       img.onload = () => {
         context.fillRect(0, 0, 320, 320);
         context.drawImage(img, 0, 0, 320, 320);
+        if (this.recorderType === 'gif') {
+          this.gif.addFrame(context, { copy: true, delay: 1000 / 60 });
+        }
       };
     });
     this.animation.addEventListener('complete', () => {
-      recorder.stop();
+      if (this.recorderType === 'webm') {
+        recorder.stop();
+      } else if (this.recorderType === 'gif') {
+        this.gif.render();
+      }
     });
   }
 
+  createGifEncoder() {
+    const gifEncoder = new Gif({
+      workers: 1,
+      // Do not use this.createObjectUrl() because it shouldn't be revoked
+      workerScript: URL.createObjectURL(workerBlob),
+      quality: 30,
+      height: 320,
+      width: 320,
+    });
+    const $vm = this;
+    gifEncoder.on('finished', function(blob) {
+      $vm.videoDataUrl = $vm.createObjectUrl(blob);
+      $vm.isRecording = false;
+      $vm.destroyGifEncoder();
+    });
+    return gifEncoder;
+  }
+  destroyGifEncoder() {
+    if (this.gif) {
+      this.gif.freeWorkers.forEach(w => w.terminate());
+      this.gif = null;
+    }
+  }
   mounted() {
     this.removeListner = dragDrop('.hello', (files) => {
       const file = files[0];
@@ -112,6 +175,11 @@ export default class HelloWorld extends Vue {
         filereader.addEventListener('load', () => this.loadAnimation(loadTgs(filereader)));
       }
     });
+  }
+  beforeDestroy() {
+    this.destroyAnimation();
+    this.isRecording = false;
+    this.destroyGifEncoder();
   }
 }
 </script>
